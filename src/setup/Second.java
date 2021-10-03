efeeb693da5e8465ecf356f8ed134467fd2366d3
nazlcelikbilek <nazlcelikbilek@gmail.com>
2021-10-03 12:24:37 -0400

    Updated second file again

--- src/setup/Second.java
+++ src/setup/Second.java
@@ -5,13 +5,14 @@ public class Second {
 
     }
     public  void m(){
+        System.out.println("first method");
 
     }
     public  void m(int a){
-
+        System.out.println("second method");
     }
 
     public void m(String s){
-        
+        System.out.println("third method");
     }
 }
