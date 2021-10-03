6424ee454ea496f2bd0efcf98875101de8ffc0fc
nazlcelikbilek <nazlcelikbilek@gmail.com>
2021-10-03 16:29:29 -0400

    added wooden spoon test

--- src/setup/Car.java
+++ src/setup/Car.java
@@ -2,18 +2,17 @@ package setup;
 
 public class Car {
 
-<<<<<<< HEAD
+
     // this is car class from mater branch.
 
     //we will have conflict
 
     //because there u=is difference text in different branches for car
-=======
+
     // this is our car from james branch
 
     // more text
 
     //more text
 
->>>>>>> james
 }
