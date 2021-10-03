14a2553cf405a4ffa75656bc1e67deb26b4f10fd
nazlcelikbilek <nazlcelikbilek@gmail.com>
2021-10-03 15:38:35 -0400

    made car in master branch

---
+++ src/setup/Car.java
@@ -0,0 +1,10 @@
+package setup;
+
+public class Car {
+
+    // this is car class from mater branch.
+
+    //we will have conflict
+
+    //because there u=is difference text in different branches for car
+}
